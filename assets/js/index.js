d447d5f771161b016b636026eaecdb332fd1842a
Fabian Mendoza <fmendoza.ospina7@outlook.com>
2024-10-16 22:32:05 -0500

    Improving status to the connection

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,7 +9,8 @@ const heightObj = select('.height');
 const orientationObj = select('.orientation');
 const levelObj = select('.level');
 const statusObj = select('.status');
-const connectionObj = select('.connection p');
+const connectionColorObj = select('.connection-color');
+const connectionStatusObj = select('.connection-status');
 
 levelObj.innerText = "not available";
 statusObj.innerText = "not available";
@@ -50,12 +51,12 @@ function getLanguage() {
 
 function getBrowser() {
     let browser = "Unknown Browser";
-    if (/Chrome/.test(userAgent) && !/Edge/.test(userAgent)) browser = "Chrome";
+    if (/Chrome/.test(userAgent) && !/Edg/.test(userAgent)) browser = "Chrome";
     else if (/Firefox/.test(userAgent)) browser = "Firefox";
     else if (/Safari/.test(userAgent) && !/Chrome/.test(userAgent)) browser = "Safari";
     else if (/MSIE|Trident/.test(userAgent)) browser = "Internet Explorer";
-    else if (/Edge/.test(userAgent)) browser = "Edge";
-    console.log(browser);
+    else if (/Edg/.test(userAgent)) browser = "Edge";
+    console.log("userAgent", userAgent);
     browserObj.innerText = browser;
 }
 
@@ -71,10 +72,17 @@ function getOrientation() {
     orientationObj.innerText = window.matchMedia("(orientation: portrait)").matches ? "Portrait" : "Landscape";
 }
 
-navigator.getBattery().then(function(battery) {
-    levelObj.innerText = `${battery.level * 100}%`;
-    statusObj.innerText = battery.charging ? "charging" : "charged";
-});
+function toggleConnection(isOnline) {
+    connectionStatusObj.innerText = isOnline ? "ON LINE" : "OFF LINE";
+    connectionColorObj.style.backgroundColor = isOnline ? "#04b931" : "#333045";
+}
+
+if ('getBattery' in navigator) {
+    navigator.getBattery().then(function(battery) {
+        levelObj.innerText = `${(battery.level * 100).toFixed(1)}%`;
+        statusObj.innerText = battery.charging ? "charging" : "charged";
+    });
+}
 
 listen('load', window, () => {
     getOS();
@@ -84,6 +92,8 @@ listen('load', window, () => {
     getWidth();
     getHeight();
     getOrientation();
+
+    toggleConnection(navigator.onLine);
 });
 
 listen('resize', window, () => {
@@ -93,10 +103,10 @@ listen('resize', window, () => {
 });
 
 listen('online', window, () => {
-    connectionObj.innerText = "ON LINE";
+    toggleConnection(true);
 });
 
 listen('offline', window, () => {
-    connectionObj.innerText = "OFF LINE";
+    toggleConnection(false);
 });
 
